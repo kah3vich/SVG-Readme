08690a7d13bf6924bcf73b3a08dd3fcba1891d97
kah3vich <nik.b20@mail.ru>
2023-03-12 12:24:14 +0300

    feat: test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,35 @@
-import { Controller, Get, Header, Query } from '@nestjs/common';
+// import { Controller, Get, Header, Query } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  @Get('/title')
-  @Header('content-type', 'image/svg+xml')
-  getTitle(@Query('title') title: string): string {
-    return AppService.getSubtitle(title);
-  }
+// @Controller()
+// export class AppController {
+//   @Get('/title')
+//   @Header('content-type', 'image/svg+xml')
+//   getTitle(@Query('title' | 'subtitle') title: string): string {
+//     if
+//     return AppService.getSubtitle(title);
+//   }
 
-  @Get('/subtitle')
-  @Header('content-type', 'image/svg+xml')
-  getSubtitle(@Query('subtitle') subtitle: string) {
-    return AppService.getSubtitle(subtitle);
+//   @Get('/subtitle')
+//   @Header('content-type', 'image/svg+xml')
+//   getSubtitle(@Query('subtitle') subtitle: string) {
+//     return AppService.getSubtitle(subtitle);
+//   }
+// }
+
+export type TReadmeParams = {
+  type: 'title' | 'subtitle' | 'description';
+};
+
+@Controller('/readme')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getUsers(@Query() params: TReadmeParams): string {
+    return this.appService.getReadme(params);
   }
 }
