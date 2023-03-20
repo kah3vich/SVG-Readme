0dfcf46e67a4e78017cc2b05de12565271378d56
kah3vich <nik.b20@mail.ru>
2023-03-20 22:39:29 +0300

    fix: type props

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,13 @@
 export type TReadmeToSvgParams = {
   type: 'title' | 'subtitle' | 'description' | 'span' | 'custom';
-  content: string | string[];
+  content: string;
   color?: string;
   size?: string | number;
   weight?: string | number;
   align?: 'left' | 'center' | 'right';
 };
 
-export type TReadmeToSvgContentParams = Omit<TReadmeToSvgParams, 'type'>;
+export type TReadmeToSvgContentParams = Omit<
+  TReadmeToSvgParams,
+  'type' | 'content'
+> & { content: string | string[] };
