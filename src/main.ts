e9fb2f4d68c9c6ebf33604013b83836cafe4fdeb
kah3vich <nik.b20@mail.ru>
2023-12-25 22:04:56 +0300

    feat: santa

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import cors from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.use(
+		cors({
+			credentials: true,
+			origin: true,
+		}),
+	);
+	await app.listen(3000);
 }
 bootstrap();
